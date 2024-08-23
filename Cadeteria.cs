72de4f5e2989b266f59ef9e68a949dba53c39d60
marta16g <martugutierrez166@gmail.com>
2024-08-23 11:30:43 -0300

    Agregado de constructor a la clase Cadeteria

--- Cadeteria.cs
+++ Cadeteria.cs
@@ -2,12 +2,16 @@ using System;
 
 namespace EspacioCadeteria
 {
-    public class Cadeteria 
+    public class Cadeteria
     {
         private string nombre;
         private ulong numero;
-
         public string Nombre { get => nombre; set => nombre = value; }
         public ulong Numero { get => numero; set => numero = value; }
+        public Cadeteria(string nombre, ulong numero)
+        {
+            this.nombre = nombre;
+            this.numero = numero;
+        }
     }
 }
