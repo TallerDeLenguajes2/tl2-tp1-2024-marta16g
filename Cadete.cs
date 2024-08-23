bcab1d897d234fc6653462d7ef535ec5f65afc4d
marta16g <martugutierrez166@gmail.com>
2024-08-23 10:43:39 -0300

    Creando clase Cadete

---
+++ Cadete.cs
@@ -0,0 +1,36 @@
+using System;
+using EspacioPedido;
+
+namespace EspacioCadete
+{
+    public class Cadete
+    {
+        private static int id;
+        private string nombre;
+        private string direccion;
+        private ulong telefono;
+        private List<Pedido> listadoDePedidos;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public ulong Telefono { get => telefono; set => telefono = value; }
+        public List<Pedido> ListadoDePedidos { get => listadoDePedidos; set => listadoDePedidos = value; }
+
+        public Cadete(string nombre, string direccion, ulong telefono)
+        {
+            id++;
+            this.nombre = nombre;
+            this.direccion = direccion;
+            this.telefono = telefono;
+            this.listadoDePedidos = new();
+        }
+        public Cadete(string nombre, string direccion, ulong telefono, List<Pedido> listadoDePedidos)
+        {
+            id++;
+            this.nombre = nombre;
+            this.direccion = direccion;
+            this.telefono = telefono;
+            this.listadoDePedidos = listadoDePedidos;
+        }
+    }
+}
