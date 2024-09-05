180c1bd3be7d61340f026ae90bfc2f35a48c408a
Martina Gutierrez <martugutierrez166@gmail.com>
2024-09-05 20:57:00 -0300

    Se agregó clase de funcionesCsv con un método que lee un archivo csv y retorna una lista con la información

---
+++ FuncionesCsv.cs
@@ -0,0 +1,28 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EspacioFuncionesCsv
+{
+    public class EspacioFuncionesCsv
+    {
+        public List<string[]> LeerArchivos(string nombreArchivo, char caracter)
+        {
+            List<string[]> archivoLeido = new();
+            string? linea = "";
+            using (FileStream miArchivo = new FileStream(nombreArchivo, FileMode.Open))
+            {
+                using (StreamReader lector = new StreamReader(miArchivo))
+                {
+                    while((linea = lector.ReadLine()) != null)
+                    {
+                        String[] fila = linea.Split(caracter);
+                        archivoLeido.Add(fila);
+                    }
+                }
+            }
+            return archivoLeido;
+        }
+    }
+}
