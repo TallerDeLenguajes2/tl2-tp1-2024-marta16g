3b0bfbb9996b4b637c9a14ec526c1570fb938ec9
“marta16g” <martugutierrez166@gmail.com>
2024-09-13 10:06:49 -0300

    Modificando método mostrarMenu

--- Gestion.cs
+++ Gestion.cs
@@ -6,17 +6,25 @@ namespace EspacioGestion
 {
     public class Gestion()
     {
-        public void Comienzo()
+
+        public int MostrarMenu()
         {
-            Console.WriteLine("Probando csv");
-            List<string[]> listaFilas = new();
-            listaFilas = FuncionesCsv.LeerArchivos("Pedido.csv", ',');
-            for (int i = 0; i < 3; i++)
+            Console.WriteLine("MENÚ");
+            Console.WriteLine("1) Ver Pedidos");
+            Console.WriteLine("2) Ver lista de cadetes");
+            Console.WriteLine("3) Asignar pedido a cadete");
+            Console.WriteLine("4) Cambiar estado de pedido");
+            return 6;
+        }
+        public void VerPedidos()
+        {
+            List<string[]> listaPedidos = new();
+            listaPedidos = FuncionesCsv.LeerArchivos("Pedido.csv", ',');
+            for (int i = 0; i < listaPedidos.Count; i++)
             {
-                for (int j = 0; j < listaFilas[i].Length; j++)
+                for (int j = 0; j < listaPedidos[i].Length; j++)
                 {
-                Console.WriteLine(listaFilas[i][j]);
-                    
+                    Console.WriteLine(listaPedidos[i][j]);
                 }
             }
         }
