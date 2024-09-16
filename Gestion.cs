26942f24c0c987f03f4270086a5348274ee83e8e
Martina Gutierrez <martugutierrez166@gmail.com>
2024-09-15 23:04:58 -0300

    Agregado de manejo de operaciones

--- Gestion.cs
+++ Gestion.cs
@@ -1,4 +1,5 @@
 using System;
+using EspacioCadete;
 using EspacioFuncionesCsv;
 using EspacioPedido;
 
@@ -7,23 +8,57 @@ namespace EspacioGestion
     public class Gestion()
     {
 
-        public bool ValidarEntrada(string input, int min, int max)
+        public static int ValidarEntrada(string input, int min, int max)
         {
-            if(int.TryParse(input, out int operacion) && operacion < max && operacion > min)
+            if (int.TryParse(input, out int operacion) && operacion < max && operacion > min)
             {
-                return true;
-            }else{
-                return false;
+                return operacion;
             }
-            
+            else
+            {
+                return 0;
+            }
+
         }
         public void MostrarMenu()
         {
             Console.WriteLine("MENÚ");
-            Console.WriteLine("1) Ver Pedidos");
-            Console.WriteLine("2) Ver lista de cadetes");
-            Console.WriteLine("3) Asignar pedido a cadete");
+            Console.WriteLine("1) Dar de alta pedidos");
+            Console.WriteLine("2) Asignar pedido a cadete");
+            Console.WriteLine("3) Reasignar pedido a cadete");
             Console.WriteLine("4) Cambiar estado de pedido");
+        }
+
+        public void ManejoDeOperaciones(int operacion)
+        {
+            switch (operacion)
+            {
+                case 1:
+                    DarDeAltaPedido();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+        }
+
+        public void DarDeAltaPedido()
+        {
+            VerPedidos();
+            Console.WriteLine("Escriba");
+        }
+
+        public void AsignarPedidoACadete(int id, Pedido pedido)
+        {
+            List<Cadete> listaCadetes = FuncionesCsv.ConvertirCadete(FuncionesCsv.LeerArchivos("Cadete.csv", ','));
+            Cadete cadeteEncontrado = listaCadetes.Find(cad => cad.Id == id);
+            if (cadeteEncontrado != null)
+            {
+                cadeteEncontrado.ListadoDePedidos.Add(pedido);
+            }
         }
         public void VerPedidos()
         {
