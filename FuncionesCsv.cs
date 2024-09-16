3f758ca62e414ae78e9e471b77c1952e45b19ec4
Martina Gutierrez <martugutierrez166@gmail.com>
2024-09-15 23:05:36 -0300

    Se agregó método para agregar linea en archivo csv y otro método para convertir un cadete

--- FuncionesCsv.cs
+++ FuncionesCsv.cs
@@ -2,6 +2,8 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Text;
+using EspacioPedido;
+using EspacioCadete;
 
 namespace EspacioFuncionesCsv
 {
@@ -15,7 +17,7 @@ namespace EspacioFuncionesCsv
             {
                 using (StreamReader lector = new StreamReader(miArchivo))
                 {
-                    while((linea = lector.ReadLine()) != null)
+                    while ((linea = lector.ReadLine()) != null)
                     {
                         String[] fila = linea.Split(caracter);
                         archivoLeido.Add(fila);
@@ -23,6 +25,28 @@ namespace EspacioFuncionesCsv
                 }
             }
             return archivoLeido;
+        }
+
+        public static void AgregarLinea(string nombreArchivo, string linea)
+        {
+            using (FileStream miArchivo = new FileStream(nombreArchivo, FileMode.Append, FileAccess.Write))
+            {
+                using (StreamWriter escritor = new StreamWriter(miArchivo))
+                {
+                    escritor.WriteLine(linea);
+                }
+            }
+        }
+
+        public static List<Cadete> ConvertirCadete(List<string[]> Filas)
+        {
+            List<Cadete> MisCadetes = new List<Cadete>();
+            foreach (string[] filas in Filas)
+            {
+                Cadete cad = new Cadete(int.Parse(filas[0]), filas[1], filas[2], ulong.Parse(filas[3]));
+                MisCadetes.Add(cad);
+            }
+            return MisCadetes;
         }
     }
 }
