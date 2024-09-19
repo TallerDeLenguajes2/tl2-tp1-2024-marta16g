23f95da09de9b1a657e24a4b2365f89bf9296a77
Martina Gutierrez <martugutierrez166@gmail.com>
2024-09-19 19:42:19 -0300

    Agregado de lista pedidos

--- Cadeteria.cs
+++ Cadeteria.cs
@@ -1,5 +1,6 @@
 using System;
 using EspacioCadete;
+using EspacioPedido;
 
 namespace EspacioCadeteria
 {
@@ -8,15 +9,18 @@ namespace EspacioCadeteria
         private string? nombre;
         private ulong numero;
         private List<Cadete>? listaCadetes;
+        private List<Pedido>? listaPedidos;
         public string? Nombre { get => nombre; set => nombre = value; }
         public ulong Numero { get => numero; set => numero = value; }
         public List<Cadete>? ListaCadetes { get => listaCadetes; set => listaCadetes = value; }
+        public List<Pedido>? ListaPedidos { get => listaPedidos; set => listaPedidos = value; }
         public Cadeteria(){}
-        public Cadeteria(string nombre, ulong numero, List<Cadete> listaCadetes)
+        public Cadeteria(string nombre, ulong numero)
         {
             this.nombre = nombre;
             this.numero = numero;
-            this.listaCadetes = listaCadetes;
+            this.listaCadetes = new();
+            this.listaPedidos = new();
         }
     }
 }
