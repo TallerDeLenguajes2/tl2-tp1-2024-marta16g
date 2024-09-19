ffdec94aab0c220c8f86d8ebd4d19ebecd1805bd
Martina Gutierrez <martugutierrez166@gmail.com>
2024-09-19 16:13:17 -0300

    Se agregó ciclo while + Se implementó la lógica switch de operación

--- Program.cs
+++ Program.cs
@@ -9,21 +9,47 @@ class Program
 {
     private static void Main(string[] args)
     {
-        Gestion gestion = new Gestion();
-        gestion.Inicializar();
+        // Gestion gestion = new Gestion();
+        // gestion.Inicializar();
+        Cadeteria miCadeteria = FuncionesCsv.ConvertirCadeteria(FuncionesCsv.LeerArchivos("csv/Cadeteria.csv", ','));
         Console.WriteLine("BIENVENIDO");
+        Console.WriteLine($"----Cadetería: {miCadeteria.Nombre}, {miCadeteria.Numero}----");
 
         int operacion;
-        do
+        while (true)
         {
-            gestion.MostrarMenu();
-            Console.WriteLine("Elija la operación:");
-            string input = Console.ReadLine();
-            operacion = Gestion.ValidarEntrada(input, 0, 5);
-        } while (operacion == 0);
 
-        gestion.ManejoDeOperaciones(operacion);
-        
+            do
+            {
+                Gestion.MostrarMenu();
+                Console.WriteLine("Elija la operación:");
+                string input = Console.ReadLine();
+                operacion = Gestion.ValidarEntrada(input, 0, 6);
+            } while (operacion == 0);
+
+            switch (operacion)
+            {
+                case 1:
+                    Gestion.DarDeAltaPedido();
+                    break;
+                case 2:
+                    Gestion.AsignarPedidoACadete(miCadeteria);
+                    break;
+                case 3:
+                    Gestion.ReasignarPedidoACadete(miCadeteria);
+                    break;
+                case 4:
+                    Gestion.CambiarEstadoAPedido();
+                    break;
+                case 5:
+                    Console.WriteLine("Nos vemos!");
+                    return;
+                default:
+                    Console.WriteLine("Opción no válida");
+                    break;
+            }
+
+        }
     }
 }
 
